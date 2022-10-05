97f8882828f4a5870095c2443440f652ed1a5d80
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-10-05 13:28:48 +0700

    A lot of things

--- backend_flask_new/components/__init__.py
+++ backend_flask_new/components/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+
+from .blueprints.post import bppost
+from .blueprints.auth import bpauth
 
 from .config import FlaskConfig as cfg 
 from .dbsettings import Session
 from components import dbmodels as dbm
-from .blueprints.post import bppost
-from .blueprints.auth import bpauth
+
 
 App = Flask(__name__)
 App.config.from_object(cfg)
 
 jwt = JWTManager(App)
 
+
+
 App.register_blueprint(bppost, url_prefix='/posts')
 App.register_blueprint(bpauth, url_prefix='/auth')
+
+
 
 @App.teardown_appcontext
 def cleanup(resp_or_exc):
