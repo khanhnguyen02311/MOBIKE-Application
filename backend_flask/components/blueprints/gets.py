3f9a0423a4af14dfd43ed53a89e75b2b790b89c1
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-11-11 04:11:19 +0700

    merge preparing

--- backend_flask/components/blueprints/gets.py
+++ backend_flask/components/blueprints/gets.py
@@ -5,7 +5,12 @@ from ..dbsettings import Session
 from ..inserter import *
 from ..dbschemas import *
 
+STEP = 100
 bpget = Blueprint('bpget', __name__)
+
+@bpget.route('/step', methods=['GET'])
+def getStep():
+    return jsonify(STEP)
 
 
 @bpget.route('/version/<name>', methods = ['GET'])
@@ -16,7 +21,6 @@ def getversions(name):
     print("Found: " + result.Name)
     return jsonify(schema.dump(result))
 
-
 @bpget.route('/permissions', methods = ['GET'])
 def getpermissions():
     schema = PermissionSchema(many=True)
@@ -24,26 +28,40 @@ def getpermissions():
     return jsonify(schema.dump(permissions))
 
 
-@bpget.route('/cities', methods = ['GET'])
-def getcities():
+@bpget.route('/count/cities/', methods = ['GET'])
+def countcities():
+    return jsonify(Session.query(City).count())
+
+@bpget.route('/cities/<fid>', methods = ['GET'])
+def getcities(fid):
+    FirstID = int(fid)
     schema = CitySchema(many=True)
-    cities = Session.query(City).all()
+    cities = Session.query(City).where(City.ID >= FirstID).limit(STEP)
     return jsonify(schema.dump(cities))
 
 
-@bpget.route('/districts', methods = ['GET'])
-def getdistricts():
+@bpget.route('/count/districts', methods = ['GET'])
+def countdistricts():
+    return jsonify(Session.query(District).count())
+
+@bpget.route('/districts/<fid>', methods = ['GET'])
+def getdistricts(fid):
+    FirstID = int(fid)
     schema = DistrictSchema(many=True)
-    districts = Session.query(District).all()
+    districts = Session.query(District).filter(District.ID >= FirstID).limit(STEP)
     return jsonify(schema.dump(districts))
 
 
-@bpget.route('/wards', methods = ['GET'])
-def getwards():
-    schema = WardSchema(many=True)
-    wards = Session.query(Ward).all()
-    return jsonify(schema.dump(wards))
+@bpget.route('/count/wards', methods = ['GET'])
+def countwards():
+    return jsonify(Session.query(Ward).count())
 
+@bpget.route('/wards/<fid>', methods = ['GET'])
+def getwards(fid):
+    firstID = int(fid)
+    schema = WardSchema(many=True)
+    wards = Session.query(Ward).filter(Ward.ID >= firstID).limit(STEP)
+    return jsonify(schema.dump(wards))
 
 @bpget.route('/imagetypes', methods = ['GET'])
 def getimagetypes():
