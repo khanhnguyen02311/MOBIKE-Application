c266179b9e1de1169aabc90bc3357dfc73bd17c9
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-11-13 22:00:35 +0700

    update for merge

--- backend_flask/components/__init__.py
+++ backend_flask/components/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
-from .blueprints.admin import bpadmin
-from .blueprints.auth import bpauth
-from .blueprints.gets import bpget
-from .blueprints.image import bpimage
-from .blueprints.post import bppost
 from .config import FlaskConfig as cfg
 
 
@@ -18,12 +13,6 @@ def create_app():
     @App.route("/")
     def hello():
         return "<h1>ABC!</h1>"
-    
-    App.register_blueprint(bppost, url_prefix='/posts')
-    App.register_blueprint(bpauth, url_prefix='/auth')
-    App.register_blueprint(bpimage, url_prefix='/image')
-    App.register_blueprint(bpget, url_prefix='/gets')
-    App.register_blueprint(bpadmin, url_prefix='/admin')
     
     return App
 
