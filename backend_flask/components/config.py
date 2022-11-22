090fb877a27ff2da94dc8fcc568f6bdc7cdf17ff
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-11-22 13:33:32 +0700

    fix config

--- backend_flask/components/config.py
+++ backend_flask/components/config.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
+import platform
 from dotenv import load_dotenv
 from os import environ
 from argon2 import Type as ArgonType
 
-#HOME_DIRECTORY = '/var/lib/jenkins/workspace/mobike-development/backend_flask/'
-HOME_DIRECTORY = '/media/knguyen02311/Data Disk/Learn Programming/ReactDEV/React Native/Mobike-application-stack/backend_flask/'
+HOME_DIRECTORY = '/var/lib/jenkins/workspace/mobike-development/backend_flask/'
 STORAGE_PATH = HOME_DIRECTORY + 'Storage/'
 
 load_dotenv(dotenv_path=HOME_DIRECTORY + '.env')
@@ -13,13 +13,23 @@ DB_USERNAME = environ.get('DBUSERNAME')
 DB_PASSWORD = environ.get('DBPASSWORD')
 DB_NAME = environ.get('DBNAME')
 
+IsTrueServer = True
+
+if (platform.system() == "Windows"):
+   IsTrueServer = False
+   DB_NAME = "flask"
+   DB_USERNAME = "root"
+   DB_PASSWORD = "123456789"
+   STORAGE_PATH = ".\Storage\\"
+
+
 class FlaskConfig:
    SECRET_KEY = environ.get('SECRET_KEY')
    JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=14)
 
 class SQLAlchemyConfig:
-   SQLALCHEMY_DATABASE_URL = "mysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost/" + DB_NAME + "?unix_socket=/var/run/mysqld/mysqld.sock"
+   SQLALCHEMY_DATABASE_URL = "mysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost/" + DB_NAME + (IsTrueServer and "?unix_socket=/var/run/mysqld/mysqld.sock" or "")
    ECHO = False
    AUTO_FLUSH = True
    AUTO_COMMIT = False
