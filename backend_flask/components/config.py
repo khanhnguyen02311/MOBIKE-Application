b8885a718a37f2fafd88e15de22c759e1370e917
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-11-26 12:09:29 +0700

    Change ImageStorage Path

--- backend_flask/components/config.py
+++ backend_flask/components/config.py
@@ -5,7 +5,7 @@ from os import environ
 from argon2 import Type as ArgonType
 
 HOME_DIRECTORY = '/var/lib/jenkins/workspace/mobike-development/backend_flask/'
-STORAGE_PATH = HOME_DIRECTORY + 'Storage/'
+STORAGE_PATH = '/var/lib/jenkins/workspace/mobike-development/ImageStorage/'
 
 load_dotenv(dotenv_path=HOME_DIRECTORY + '.env')
 
