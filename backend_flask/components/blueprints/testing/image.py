b742e9938c000931cf5dda63a37c7d522ad0b5d8
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-11-28 18:02:20 +0700

    test

--- backend_flask/components/blueprints/testing/image.py
+++ backend_flask/components/blueprints/testing/image.py
@@ -30,7 +30,7 @@ def upload():
         # test = Session.query(dbm.Image).get(new_image.ID)
         # if (test is new_image):
         #     return jsonify({'msg': 'Success'}), 200
-        new_image.Filename = str(new_image.ID) + '.' + ext
+        # new_image.Filename = str(new_image.ID) + '.' + ext
         # Session.flush()
         # Session.refresh(new_image)
         f.save(os.path.join(STORAGE_PATH, str(new_image.ID) + '.' + ext))
