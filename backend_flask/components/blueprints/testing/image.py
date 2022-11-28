0697538340bec57f45bd12dada5c2b8b859909cd
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-11-28 18:07:22 +0700

    Update image.py

--- backend_flask/components/blueprints/testing/image.py
+++ backend_flask/components/blueprints/testing/image.py
@@ -25,7 +25,7 @@ def upload():
         new_image = dbm.Image(Filename=f.filename)
         Session.add(new_image)
         Session.flush()
-        Session.refresh(new_image)
+        # Session.refresh(new_image)
         # return jsonify({'msg': new_image.ID}), 200
         # test = Session.query(dbm.Image).get(new_image.ID)
         # if (test is new_image):
