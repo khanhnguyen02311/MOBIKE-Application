44f5ab6bb78a0885c9a6e64cbabad3a4e6d1248d
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-11-28 16:34:00 +0700

    test

--- backend_flask/components/blueprints/testing/image.py
+++ backend_flask/components/blueprints/testing/image.py
@@ -16,22 +16,18 @@ def upload():
     if 'file' not in request.files:
         return jsonify({'msg': 'No file part'}), 400
     f = request.files['file']
+    f.read()
+    if f.filename == '':
+        return jsonify({'msg': 'No selected file'}), 400
+    ext = f.filename.split('.')[-1]
     new_image = dbm.Image()
-    
     new_image.Filename = f.filename
     Session = new_Session()
     try:
-        f.read()
-        if f.filename == '':
-            Session.commit()
-            Session.close()
-            return jsonify({'msg': 'No selected file'}), 400
-        ext = f.filename.split('.')[-1]
         Session.add(new_image)
         Session.flush()
         Session.refresh(new_image)
         new_image.Filename = str(new_image.ID) + '.' + ext
-        Session.merge(new_image)
         Session.flush()
         Session.refresh(new_image)
         f.save(os.path.join(STORAGE_PATH, new_image.Filename))
