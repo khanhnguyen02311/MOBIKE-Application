e64f815d2d8c8718374a954935f33680d9c16369
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-12-04 17:55:45 +0700

    Try SocketIO

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -1,7 +1,8 @@
-from components import create_app
+from components import Socketio, App
 
-App = create_app() 
+# App = create_app()
 
 if __name__ == "__main__":
-    App.run(debug=True)
-    
+    # App.run(debug=True)
+    Socketio.run(App, debug=True)
+
