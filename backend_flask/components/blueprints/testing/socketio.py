5c3c97cd593bdd22d8b311f7f72b7ab769252637
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-12-04 18:06:14 +0700

    .

--- backend_flask/components/blueprints/testing/socketio.py
+++ backend_flask/components/blueprints/testing/socketio.py
@@ -1,7 +1,2 @@
 from components import Socketio, App
 
-@Socketio.on('connect')
-def handle_message(message):
-    print('received message: ' + message)
-    if message != 'User connected!':
-        Socketio.send(message, broadcast=True)
--- backend_flask/main.py
+++ backend_flask/main.py
@@ -6,3 +6,8 @@ if __name__ == "__main__":
     # App.run(debug=True)
     Socketio.run(App, debug=True)
 
+@Socketio.on('connect')
+def handle_message(message):
+    print('received message: ' + message)
+    if message != 'User connected!':
+        Socketio.send(message, broadcast=True)
