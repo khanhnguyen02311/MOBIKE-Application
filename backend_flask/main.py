a0b007e4ddd6c1e8285cb9293294ba69f1d27021
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2022-12-05 10:43:56 +0700

    Update main.py

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -1,13 +1,12 @@
 from components import Socketio, App
 
 # App = create_app()
-
-if __name__ == "__main__":
-    # App.run(debug=True)
-    Socketio.run(App, debug=True)
-
-@Socketio.on('connect')
+@Socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
     if message != 'User connected!':
         Socketio.send(message, broadcast=True)
+
+if __name__ == "__main__":
+    # App.run(debug=True)
+    Socketio.run(App, debug=True)
