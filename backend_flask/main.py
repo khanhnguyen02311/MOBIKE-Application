175ff60f0c1401847fd4fe47ad59111996eb1b71
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-12-05 13:04:37 +0700

    Revert "Update main.py"
    This reverts commit a0b007e4ddd6c1e8285cb9293294ba69f1d27021.

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -1,12 +1,13 @@
 from components import Socketio, App
 
 # App = create_app()
-@Socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-    if message != 'User connected!':
-        Socketio.send(message, broadcast=True)
 
 if __name__ == "__main__":
     # App.run(debug=True)
     Socketio.run(App, debug=True)
+
+@Socketio.on('connect')
+def handle_message(message):
+    print('received message: ' + message)
+    if message != 'User connected!':
+        Socketio.send(message, broadcast=True)
