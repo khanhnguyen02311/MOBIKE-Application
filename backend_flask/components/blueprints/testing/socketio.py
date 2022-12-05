ade8e6738cebf2b656d35ec52ff3ee92b72f8b49
Khanh Nguyen <khanhnguyen02311@gmail.com>
2022-12-05 13:04:48 +0700

    Revert "."
    This reverts commit 5c3c97cd593bdd22d8b311f7f72b7ab769252637.

--- backend_flask/components/blueprints/testing/socketio.py
+++ backend_flask/components/blueprints/testing/socketio.py
@@ -1,2 +1,7 @@
 from components import Socketio, App
 
+@Socketio.on('connect')
+def handle_message(message):
+    print('received message: ' + message)
+    if message != 'User connected!':
+        Socketio.send(message, broadcast=True)
--- backend_flask/main.py
+++ backend_flask/main.py
@@ -6,8 +6,3 @@ if __name__ == "__main__":
     # App.run(debug=True)
     Socketio.run(App, debug=True)
 
-@Socketio.on('connect')
-def handle_message(message):
-    print('received message: ' + message)
-    if message != 'User connected!':
-        Socketio.send(message, broadcast=True)
