5350802ceabd8a421d19573215285242820f2007
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-01-09 16:41:24 +0700

    test

--- backend_flask/components/blueprints/personal/account.py
+++ backend_flask/components/blueprints/personal/account.py
@@ -53,7 +53,7 @@ def getinfo2():
    try:
       # acc = Session.query(dbm.Account.ID, dbm.AccountInfo.Name, dbm.AccountInfo.Phone_number, dbm.AccountInfo.Identification_number, dbm.AccountInfo.Birthdate).join(dbm.AccountInfo, dbm.Account.ID == dbm.AccountInfo.ID)
       acc = Session.query(dbm.Account).options(sqlorm.joinedload(dbm.Account.rel_AccountInfo)).get(current_user['ID'])
-      return acc
+      return jsonify({"message": "Completed", "error": "", "info": acc})
       if acc == None:
          Session.close()
          return jsonify({"message": "Incompleted", "error": "Account not found", "info": acc})
