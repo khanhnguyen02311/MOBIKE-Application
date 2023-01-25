ed1b242f87df61b5c26fe27bfab6363510f20fd6
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-01-25 22:52:27 +0700

    Update image.py

--- backend_flask/components/blueprints/testing/image.py
+++ backend_flask/components/blueprints/testing/image.py
@@ -75,6 +75,7 @@ def upload(imageTypeID: int):
             new_image = dbm.Image(Filename = "blabla", ID_ImageType=imageTypeID)
             Session.add(new_image)
             Session.flush()
+            Session.refresh(new_image)
             new_image.FileName = str(new_image.ID) + '.' + ext
             Session.commit()
 
