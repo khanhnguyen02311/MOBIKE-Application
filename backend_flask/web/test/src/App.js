d567f268b0f4ddee927ae6799abf14ab537b606c
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-02-06 04:32:26 +0700

    Final mess :100:

--- backend_flask/web/test/src/App.js
+++ backend_flask/web/test/src/App.js
@@ -9,26 +9,16 @@ function App() {
           src="https://abcdavid-knguyen.ddns.net:30001/image/get/186"
           className="App-logo"
           alt="KNguyen"
-
         />
         <a
           className="App-link"
           href="https://www.facebook.com/khanhkhanh66"
           target="_blank"
           rel="noopener noreferrer"
-          style={{ margin: '0px 0px 30px 0px' }}
+          style={{ margin: '50px 0px 0px 0px' }}
         >
           Nguyen Khanh
         </a>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/y_fB0IMbq54"
-          frameborder="100"
-          // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen>
-
-        </iframe>
       </header>
     </div>
   );
