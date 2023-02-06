36e23da21f307f2a9651939df30b23299f22a4e9
Khanh Nguyen <khanhnguyen02311@gmail.com>
2023-02-07 04:02:37 +0700

    user search functions

--- backend_flask/components/blueprints/search/postsearch.py
+++ backend_flask/components/blueprints/search/postsearch.py
@@ -7,44 +7,45 @@ from components import dbmodels as dbm, dbschemas as dbs
 bppostsearch = Blueprint("bppostsearch", __name__)
 
 
-# @bppostsearch.route("/post/search", methods=["POST"])
-# def searchposts():
-#    arg_searchstr = request.args.get('string', default = "", type = str)
-#    arg_page = request.args.get('page', default = 1, type = int)
-#    arg_numperpage = request.args.get('numperpage', default = 20, type = int)
-#    arg_sortby = request.args.get('sortby', default = "", type = str)
-#    arg_order = request.args.get('order', default = "asc", type = str)
-#    arg_pricestart = request.args.get('pricestart', default = -1, type = int)
-#    arg_priceend = request.args.get('priceend', default = -1, type = int)
-#    arg_type = request.args.get('type', default = -1, type = int)
-#    arg_brand = request.args.get('brand', default = -1, type = int)
-#    arg_lineup = request.args.get('lineup', default = -1, type = int)
-#    arg_color = request.args.get('color', default = -1, type = int)
-#    arg_mnfyear = request.args.get('mnfyear', default = -1, type = int)
+@bppostsearch.route("/post/all", methods=["GET"])
+def searchposts():
+   arg_searchstr = request.args.get('string', default = "", type = str)
+   arg_page = request.args.get('page', default = 1, type = int)
+   arg_numperpage = request.args.get('numperpage', default = 20, type = int)
+   arg_sortby = request.args.get('sortby', default = "", type = str)
+   arg_order = request.args.get('order', default = "asc", type = str)
+   arg_pricestart = request.args.get('pricestart', default = -1, type = int)
+   arg_priceend = request.args.get('priceend', default = -1, type = int)
+   arg_type = request.args.get('type', default = -1, type = int)
+   arg_brand = request.args.get('brand', default = -1, type = int)
+   arg_lineup = request.args.get('lineup', default = -1, type = int)
+   arg_color = request.args.get('color', default = -1, type = int)
+   arg_mnfyear = request.args.get('mnfyear', default = -1, type = int)
    
-#    Session = new_Scoped_session()
-#    try:
+   Session = new_Scoped_session()
+   try:
       
-#       if arg_sortby == "rating": query_orderby = dbm.Post.rel_Rating
-#       elif arg_sortby == "like": query_orderby = dbm.Post.rel_Like
-#       else: query_orderby = dbm.Post.ID
+      if arg_sortby == "rating": query_orderby = dbm.Post.rel_Rating
+      elif arg_sortby == "like": query_orderby = dbm.Post.rel_Like
+      else: query_orderby = dbm.Post.ID
       
-#       query_orderby = query_orderby.asc() if arg_order == "asc" else query_orderby.desc()
+      query_orderby = query_orderby.asc() if arg_order == "asc" else query_orderby.desc()
       
-#       posts = Session.query(dbm.Post
-#          ).join(dbm.Post.rel_VehicleInfo
-#          ).filter(arg_type == -1 or dbm.VehicleInfo.ID_VehicleType == arg_type, 
-#                   arg_brand == -1 or dbm.VehicleInfo.ID_VehicleBrand == arg_brand, 
-#                   arg_lineup == -1 or dbm.VehicleInfo.ID_VehicleLineup == arg_lineup, 
-#                   arg_color == -1 or dbm.VehicleInfo.ID_Color == arg_color,
-#                   arg_mnfyear == -1 or dbm.VehicleInfo.Manufacture_year == arg_mnfyear)
+      posts = Session.query(dbm.Post
+         ).join(dbm.Post.rel_VehicleInfo
+         ).filter(arg_type == -1 or dbm.VehicleInfo.ID_VehicleType == arg_type, 
+                  arg_brand == -1 or dbm.VehicleInfo.ID_VehicleBrand == arg_brand, 
+                  arg_lineup == -1 or dbm.VehicleInfo.ID_VehicleLineup == arg_lineup, 
+                  arg_color == -1 or dbm.VehicleInfo.ID_Color == arg_color,
+                  arg_mnfyear == -1 or dbm.VehicleInfo.Manufacture_year == arg_mnfyear)
+         
                                              
-#       Session.commit()
-#       return jsonify({"msg": "Completed", "error": "", "info": posts})
+      Session.commit()
+      return jsonify({"msg": "Completed", "error": "", "info": posts})
       
-#    except Exception as e:
-#       Session.rollback()
-#       return jsonify({"msg": "Incompleted", "error": str(e), "info": ""})
+   except Exception as e:
+      Session.rollback()
+      return jsonify({"msg": "Incompleted", "error": str(e), "info": ""})
 
 
 @bppostsearch.route("/post/<id>", methods=["GET"])
