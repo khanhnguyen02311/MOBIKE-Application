0ceacfb7335901bb87b5953bf570c17148f60bab
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-02-07 06:52:53 +0700

    Update validation.py

--- backend_flask/components/blueprints/admin/application/validation.py
+++ backend_flask/components/blueprints/admin/application/validation.py
@@ -5,14 +5,16 @@ import flask_jwt_extended as jwte
 from components.dbsettings import new_Scoped_session
 from components import dbmodels as dbm, dbschemas as dbs
 from components.security import check_hash, make_hash
+import sqlalchemy.orm as sqlorm
+from sqlalchemy import desc
 
 bpappadminval = Blueprint('bpappadminval', __name__)
 
 
-@bpappadminval.route('/status/set', methods = ['POST'])
+@bpappadminval.route('/status/set', methods=['POST'])
 @jwt_required()
 def setpoststatus():
-   current_user = get_jwt_identity()   
+   current_user = get_jwt_identity()
    if current_user is None:
       return jsonify({"msg": "Incompleted", "error": "Invalid token", "info": ""})
 
@@ -25,16 +27,95 @@ def setpoststatus():
       if acc.ID_Permission > 2:
          Session.close()
          return jsonify({"msg": "Incompleted", "error": "No permission", "info": ""})
-      
+
       info = request.get_json()
       new_status = dbm.PostStatus(
-         Status = info['status'],
-         Information = info['info'],
-         ID_Post = info['post']
+         Status=info['status'],
+         Information=info['info'],
+         ID_Post=info['post']
       )
       Session.add(new_status)
       Session.commit()
       return jsonify({"msg": "Completed", "error": "", "info": new_status.ID})
+
+   except Exception as e:
+      Session.rollback()
+      return jsonify({"msg": "Incompleted", "error": str(e), "info": ""})
+
+
+@bpappadminval.route('/inactiveposts', methods=['GET'])
+@jwt_required()
+def getinactivapost():
+   current_user = get_jwt_identity()
+   if current_user is None:
+      return jsonify({"msg": "Incompleted", "error": "Invalid token", "info": ""})
+
+   Session = new_Scoped_session()
+   try:
+      acc = Session.query(dbm.Account).get(current_user['ID'])
+      if acc == None:
+         Session.close()
+         return jsonify({"msg": "Incompleted", "error": "Account not found", "info": ""})
+      if acc.ID_Permission > 2:
+         Session.close()
+         return jsonify({"msg": "Incompleted", "error": "No permission", "info": ""})
+
+      last_statuses = (
+         Session.query(dbm.PostStatus)
+         .join(dbm.Post)
+         .group_by(dbm.Post.ID)
+         .order_by(desc(dbm.PostStatus.Time_updated))
+         .filter(dbm.PostStatus.ID_Post == dbm.Post.ID)
+      )
+
+      schema = dbs.PostSchema(many=True)
+      
+      return jsonify({"msg": "Completed", "error": "", "info": schema.dump(last_statuses)})
+
+   except Exception as e:
+      Session.rollback()
+      return jsonify({"msg": "Incompleted", "error": str(e), "info": ""})
+
+@bpappadminval.route('/post/<int:id>', methods = ['GET'])
+@jwt_required()
+def getPost(id):
+   current_user = get_jwt_identity()
+   if current_user is None:
+      return jsonify({"msg": "Incompleted", "error": "Invalid token", "info": ""})
+   
+   Session = new_Scoped_session()
+   try:
+      acc = Session.query(dbm.Account).get(current_user['ID'])
+      if acc == None:
+         Session.close()
+         return jsonify({"msg": "Incompleted", "error": "Account not found", "info": ""})
+      if acc.ID_Permission > 2:
+         Session.close()
+         return jsonify({"msg": "Incompleted", "error": "No permission", "info": ""})
+      
+      postschema = dbs.PostSchema()
+      vehicleschema = dbs.VehicleInfoSchema()
+      userschema = dbs.AccountInfoSchemaPublic()
+      addressschema = dbs.AddressSchemaShort()
+      
+      status = Session.query(dbm.PostStatus).filter(dbm.PostStatus.ID_Post == id).order_by(dbm.PostStatus.ID.desc()).first()
+      if status == None:
+         return jsonify({"msg": "Completed", "error": "Post not found", "info": ""})
+      
+      post = Session.query(dbm.Post).options(sqlorm.joinedload(dbm.Post.rel_VehicleInfo),
+                                             sqlorm.joinedload(dbm.Post.rel_Account).subqueryload(dbm.Account.rel_AccountInfo),
+                                             sqlorm.joinedload(dbm.Post.rel_Image),
+                                             sqlorm.joinedload(dbm.Post.rel_Like),
+                                             sqlorm.joinedload(dbm.Post.rel_Rating),
+                                             sqlorm.joinedload(dbm.Post.rel_Address)).get(id)
+      
+      json_data = {}
+      json_data['post'] = postschema.dump(post)
+      json_data['user'] = userschema.dump(post.rel_Account.rel_AccountInfo)
+      json_data['vehicleinfo'] = vehicleschema.dump(post.rel_VehicleInfo)
+      json_data['address'] = addressschema.dump(post.rel_Address)
+      Session.close()
+      return jsonify({"msg": "Completed", "error": "", "info": json_data})
 
    except Exception as e:
       Session.rollback()
