a8b3367ef70c8b6c5f07ac316eae03d54a90ab13
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-02-07 12:50:40 +0700

    Add several apis and redux likeSlice

---
+++ frontend_reactnative/src/redux/slice/likeSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    LikedPosts: [],
+}
+
+const LikeSlice = createSlice({
+    name: 'like',
+    initialState,
+    reducers: {
+        setLikedPosts: (state, action) => {
+            state.LikedPosts = action.payload;
+        }
+    }
+});
+
+export const { setLikedPosts } = LikeSlice.actions
+
+export default LikeSlice.reducer
