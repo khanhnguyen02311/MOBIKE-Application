5edf2584b5a3ecbe7b9f039bda93fabb63172c3c
knguyen02311 <khanhnguyen02311@gmail.com>
2023-09-14 16:52:37 +0700

    Edit dockerfiles

--- backend_flask/components/config.py
+++ backend_flask/components/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 from os import environ
 from argon2 import Type as ArgonType
 
-HOME_DIRECTORY = '/var/lib/jenkins/workspace/mobike-development/backend_flask/'
-STORAGE_PATH = '/var/lib/jenkins/workspace/mobike-development/MobikeStorage/'
+HOME_DIRECTORY = '/app'
+STORAGE_PATH ='/app/storage'
 
-load_dotenv(dotenv_path=HOME_DIRECTORY + '.env')
+load_dotenv('/app/.env')
 
 DB_USERNAME = environ.get('DBUSERNAME')
 DB_PASSWORD = environ.get('DBPASSWORD')
@@ -15,12 +15,12 @@ DB_NAME = environ.get('DBNAME')
 
 IsTrueServer = True
 
-if (platform.system() == "Windows"):
-   IsTrueServer = False
-   DB_NAME = "flask"
-   DB_USERNAME = "root"
-   DB_PASSWORD = "123456789"
-   STORAGE_PATH = ".\Storage\\"
+# if (platform.system() == "Windows"):
+#    IsTrueServer = False
+#    DB_NAME = "flask"
+#    DB_USERNAME = "root"
+#    DB_PASSWORD = "123456789"
+#    STORAGE_PATH = ".\Storage\\"
 
 
 class FlaskConfig:
@@ -30,7 +30,8 @@ class FlaskConfig:
    DEBUG_TOOLBAR_ENABLED = True
 
 class SQLAlchemyConfig:
-   SQLALCHEMY_DATABASE_URL = "mysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost/" + DB_NAME + (IsTrueServer and "?unix_socket=/var/run/mysqld/mysqld.sock&charset=utf8mb4" or "")
+   SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost:3306/" + DB_NAME + "?unix_socket=/var/run/mysqld/mysqld.sock&charset=utf8mb4" 
+   # (IsTrueServer and "?unix_socket=/var/run/mysqld/mysqld.sock&charset=utf8mb4" or "")
    ECHO = False
    AUTO_FLUSH = True
    AUTO_COMMIT = False
--- backend_flask/main.py
+++ backend_flask/main.py
@@ -4,7 +4,4 @@ App = create_app()
 
 if __name__ == "__main__":
     print(App.url_map)
-    App.run(host='0.0.0.0', debug=True, threaded=True)
-
-    # todo:
-    # reset table POSTSTATUS
+    App.run(host='0.0.0.0', port=10000, debug=False, threaded=True)
